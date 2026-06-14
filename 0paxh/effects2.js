bd59f695f8ab680a8ba22c13afde4008a96ce616
RainyXyz <revnash713@gmail.com>
2026-06-14 23:20:53 +0800

    Add files via upload

---
+++ 0paxh/effects2.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const imgSrc = "https://lanyard.cnrad.dev/api/984260724165603338?bg=00000000&hideSpotify=true&showDisplayName=true";
+    const imgElement = document.getElementById('rpc');
+    imgElement.src = imgSrc;
+});
